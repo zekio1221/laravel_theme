2879ce0de3c810edfc53c828c24a0207478f2df2
daikiweb1221 <daiki.web1221@gmail.com>
2023-04-14 19:26:51 +0900

    contactsテーブル作成

---
+++ src/database/migrations/2023_04_14_101550_create_contacts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 20);
+            $table->string('email', 255);
+            $table->string('content', 1000);
+            $table->integer('age')->nullable();
+            $table->integer('gender')->nullable();
+            $table->unsignedBigInteger('department_id');
+            $table->timestamps();
+
+            $table->foreign('department_id')
+                ->references('id')
+                ->on('departments')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
